40f6ce4a21f2075a94cdb28985b8d977fdb87d4e
Tailon <renewedT2016@gmail.com>
2020-02-05 16:36:06 -0900

    Add the current tests

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include <string>
+#include <sstream>
 
 using std::string;
 
@@ -11,7 +12,36 @@ int strings(string dummy) {
 	}
 	else
 	{
-		return dummy[0] - 48; //ASCII 
+		std::istringstream num(dummy);
+		string x;
+		int total = 0;
+		if (num.str().length() == 1)
+		{
+			int one;
+			num >> one;
+			return one;
+		}
+		else
+		{
+			for (int i = 0; i < num.str().length(); i++)
+			{
+				if (num.str()[i] != ',')
+				{
+					x += num.str()[i];
+				}
+				else
+				{
+					dummy = x;
+					x = "";
+					continue;
+				}
+			}
+			std::istringstream num1(x);
+			int one;
+			num1 >> one;
+			total += one;
+		}
+		return total;
 	}
 };
 
@@ -23,4 +53,10 @@ TEST_CASE("A single number returns the value") {
 	CHECK(strings("1") == 1);
 	CHECK(strings("2") == 2);
 	CHECK(strings("10") == 10);
+	CHECK(strings("100") == 100);
+	CHECK(strings("1000") == 1000);
+	CHECK(strings("0,1") == 1);
+	CHECK(strings("1,2") == 3);
+	CHECK(strings("10,0") == 10);
+	CHECK(strings("10,1") == 11);
 }
